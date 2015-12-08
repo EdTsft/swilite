89dcd9f647e7f37837cdde5724334d1c793447b3
Eric Langlois <eric@langlois.xyz>
2015-12-08 04:33:05 -0500

    Added swilite/extras.py with helpful functions.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='swilite',
-    version='0.1.0',
+    version='0.1.1',
     author='Eric Langlois',
     author_email='eric@langlois.xyz',
     license='MIT',
