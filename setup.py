201323b695881e65bdbb8e52773528f67c1cfead
Eric Langlois <eric@langlois.xyz>
2015-12-08 04:22:57 -0500

    Created setup.py for swilite.
    Version 0.1.0

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='swilite',
+    version='0.1.0',
+    author='Eric Langlois',
+    author_email='eric@langlois.xyz',
+    license='MIT',
+    keywords='Prolog SWI-Prolog',
+    url='https://github.com/EdTsft/swilite',
+    packages=['swilite'],
+    description='A light-weight object-oriented interface to SWI-Prolog.',
+)
