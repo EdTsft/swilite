998c98124efe8facfb2e61ac36b36f0b4b2fcb17
Eric Langlois <eric@langlois.xyz>
2016-05-29 16:15:51 -0400

    Incremented version number to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='swilite',
-    version='0.1.1',
+    version='0.2.1',
     author='Eric Langlois',
     author_email='eric@langlois.xyz',
     license='MIT',
